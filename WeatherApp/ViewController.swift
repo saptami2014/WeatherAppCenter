a5f2c0974fa724531bcb4f5063fc2769b2c5d1fb
Saptami Biswas <saptami.biswas@okstate.edu>
2019-04-26 08:53:00 -0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Saptami Biswas on 4/26/19.
+//  Copyright © 2019 Saptami Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
